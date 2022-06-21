a95ebf7334c1915f7b4a1d7bc63011dfeae61020
”konstantin <vihroff@gmail.com>
2022-06-21 13:47:37 +0300

    Initial commit

---
+++ src/main/java/ru/kostar/springcourse/models/Person.java
@@ -0,0 +1,57 @@
+package ru.kostar.springcourse.models;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+
+public class Person {
+    private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    private String name;
+
+    @Min(value = 0, message = "Age should be greater than 0")
+    private int date;
+
+
+
+    public Person() {
+
+    }
+
+    public Person(int id, String name, int date) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+
+}
